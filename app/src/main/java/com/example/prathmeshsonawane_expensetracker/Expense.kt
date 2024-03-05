4fa61b7d2807f4de59cadb68a9d31d41947637fa
PrathmeshS <prs.lowell@gmail.com>
2024-03-05 10:04:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/prathmeshsonawane_expensetracker/Expense.kt
@@ -0,0 +1,17 @@
+package com.example.prathmeshsonawane_expensetracker
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "expenses")
+data class Expense(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val date: Date,
+    val amount: Double,
+    val category: String
+)
+
+
+
